33e57a00fa0c8a935366db5224825c869f2c1a0d
Vladimir Slysar <g7au2ss@gmail.com>
2020-06-28 18:46:26 +0700

    comlite print

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
-    #write your code below this line
-
+    strr = input("Give a string: ")
+    intr = int(input("Give an integer: "))
+    floatr = float(input("Give a float: "))
+    boolr = bool(input("Give a boolean: "))
+    print("You gave the string " + strr)
+    print("You gave the integer " + str(intr))
+    print("You gave the float " + str(floatr))
+    print("You gave the boolean " + str(boolr))
 if __name__ == '__main__':
     main()
